cdb6708b2ed095e72e99c8880c3229cfd29d6fc6
Aryaman Bhardwaj <158012719+aryamanB0506@users.noreply.github.com>
2026-07-16 11:47:01 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<List<Integer>, List<String>> frequencyLists = new HashMap<>();
+        int[] count = new int[26];
+        List<Integer> keyNums = new ArrayList<>();
+        for(int i = 0; i < strs.length; i++){
+            for(int k = 0; k < count.length; k++){
+                count[k] = 0;
+            }
+            for(char c : strs[i].toCharArray()){
+                int index = (c - 'a');
+                count[index]++;
+            }
+            keyNums.clear();
+            for(int k = 0; k < count.length; k++){
+                keyNums.add(count[k]);
+            }
+            if(frequencyLists.containsKey(keyNums)){
+                frequencyLists.get(keyNums).add(strs[i]);
+            }
+            else{
+                List<String> current = new ArrayList<>();
+                current.add(strs[i]);
+                frequencyLists.put(keyNums, current);
+            }
+        }
+        return new ArrayList<>(frequencyLists.values());
+    }
+}
