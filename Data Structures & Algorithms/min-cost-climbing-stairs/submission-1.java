1b5b34355caaecb245d9ee0ef8b0e309fab49055
Aryaman Bhardwaj <158012719+aryamanB0506@users.noreply.github.com>
2026-07-20 12:24:53 -0500

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length+1];
+        
+        for(int i = 0; i < cost.length; i++){
+            if(i==0){
+                dp[0] = cost[0];
+            }
+            else if(i==1){
+                dp[1] = cost[1];
+            }
+            else{
+                System.out.println("Setting index " + i);
+                dp[i] = Math.min(dp[i-1] + cost[i], dp[i-2] + cost[i]);
+                System.out.println("Value is " + dp[i]);
+            }
+        }
+        dp[cost.length] = Math.min(dp[cost.length-1], dp[cost.length-2]);
+        System.out.println("Comparing -1 " + dp[cost.length-1] + " and -2 " + dp[cost.length-2] + " at " + (cost.length-2));
+        return dp[cost.length];
+    }
+}
