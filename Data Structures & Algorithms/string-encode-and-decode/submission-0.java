3fa0695f50e446671747a56c484a473a30a91a0a
Aryaman Bhardwaj <158012719+aryamanB0506@users.noreply.github.com>
2026-07-21 15:08:49 -0500

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder output = new StringBuilder(); 
+        for(String str: strs){
+            output.append(str.length());
+            output.append(",");
+        }
+        output.append("#");
+        for(String str: strs){
+            output.append(str);
+        }
+        System.out.println(output.toString());
+        return output.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> output = new ArrayList<>();
+        int breaker = 0;
+        int count = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i)=='#'){
+                breaker = i;
+                break;
+            }
+            if(str.charAt(i)==','){
+                count++;
+            }
+        }
+        int start = 0;
+        int end = 0;
+        for(int i = 0; i < count; i++){
+            while(str.charAt(end)!=','){
+                System.out.println(str.charAt(end) + " " + i);
+                end++;
+            }
+            int length = Integer.parseInt(str.substring(start, end));
+            System.out.println("\"" + length + "\" Length");
+            output.add(str.substring(breaker+1, breaker+length+1));
+            System.out.println("String is \"" + str.substring(breaker+1, breaker+length+1) + "\" with " + (breaker+1) + " " + (breaker+length+1));
+            breaker = breaker+length;
+            start = end+1;
+            end++;
+        }
+        return output;
+    }
+}
